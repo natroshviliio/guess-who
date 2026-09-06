7ea150c570e46bbb57ff809c08ad43f58eb26d7b
natroshviliio <ioane.natroshvili08b@geolab.edu.ge>
2026-09-07 03:14:46 +0400

    who is

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ function App() {
         await axios.get(API_URL);
     };
 
+    void whois;
+
     useEffect(() => {
-        whois();
+        // whois();
     }, []);
 
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="flex relative justify-center">
-                <div className="absolute font-bold text-2xl top-5">You've Been Leaked!</div>
+                <div className="absolute font-bold text-2xl top-5">Here you go!</div>
                 <img src="./tusk.jpg" alt="tusk" className="w-md" />
             </div>
         </div>
